8dc213534f76112b6cd0cdcfdcb840c3a85a6446
huantransy@hotmail.com <huantransy@hotmail.com>
2023-11-21 03:53:23 +1030

    Update pointer
    ReplaceString

--- C++/Pointer/StringManipulation.cpp
+++ C++/Pointer/StringManipulation.cpp
@@ -98,16 +98,73 @@ void subString(arrayStr& str,int startPos, int len)
     delete[] str;
     str = newStr;
 }
+void replaceString(arrayStr& source,arrayStr target, arrayStr replaceText)
+{
+    int targetLen = length(target);
+    int sourceLen = length(source);
+    int startPos = -1;
+    int endPos  = -1;
+    for(int i = 0; i < sourceLen;i++)
+    {
+        for(int j = 0; j < targetLen;j++)
+        {
+            if(source[i + j] == target[j])
+            {
+                if(startPos == -1)
+                {
+                    startPos = endPos = (i + j);
+                }
+                else{
+                    endPos++;
+                }
+            }
+        }
+        if((endPos - startPos + 1) == targetLen)
+        {
+            break;
+        }
+        else
+        {
+            startPos = endPos = -1;
+        }
+    }
+   
+    if((endPos - startPos + 1) == targetLen)
+    {
+        int replaceLen = length(replaceText);
+        int newLen = replaceLen + sourceLen;
+        arrayStr newStr = new char[newLen + 1];
+        for(int i = 0; i < startPos;i++)
+        {
+            newStr[i] = source[i];
+        }
+        for(int i = startPos; i < startPos + replaceLen;i++)
+        {
+            newStr[i] = replaceText[ i - startPos];
+        }
+        for(int i = endPos + 1; i < sourceLen;i++)
+        {
+            newStr[i] = source[i];
+        }
+        
+        newStr[newLen + 1] = 0;
+        delete[] source;
+        source = newStr;
+    }
+}
 int main()
 {
     arrayStr a = new char[5];
     a[0] = 't';a[1] = 'e';a[2] = 's';a[3] = 't';a[4] = 0;
     arrayStr b = new char[4];
     b[0] = 'b';b[1] = 'e';b[2] = 'd';b[3] = 0;
+    arrayStr d = new char[5];
+     d[0] = 't';d[1] = 'e';d[2] = 's';d[3] = 't';d[4] = 0;
+    arrayStr c = new char[5];
+     c[0] = 'r';c[1] = 'o';c[2] = 'o';c[3] = 'm';c[4] = 0;
     concatenate(a,b);
-    cout << "len: " << length(a) << endl;
     cout << a << "\n" << b << "\n";
-    subString(a,0,4);
+    replaceString(a,d,c);
     cout << a << "\n";
     return 0;
 }
